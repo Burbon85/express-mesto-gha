7c99ad695b65b2eb9862e9cef3b978b920c975cd
Artur <dr.burbon85@yandex.ru>
2023-07-14 19:02:36 +0300

    13

--- app.js
+++ app.js
@@ -10,12 +10,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+// импортируем роутеры
 const userRouter = require('./routes/user');
 const cardRouter = require('./routes/card');
 
 app.use(express.json());
 app.use(userRouter);
 app.use(cardRouter);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64b170ade2d027c8bd54129b',
+  };
+  next();
+});
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'URL не существует' });
